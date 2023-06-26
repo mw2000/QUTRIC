f3f0d2b76820ef1879a9dce704d85501049305e2
Mihir Wadekar <mwadekar2000@gmail.com>
2023-06-26 00:50:15 -0700

    Example star and plaquette stabilizers

--- example_circuit.py
+++ example_circuit.py
@@ -1,16 +1,76 @@
 import stim
 
-# Define a circuit
-circuit = stim.Circuit()
+def generate_2x2_toric_code():
+    circuit = stim.Circuit()
 
+    # Qubits are laid out in a 2D grid like this:
+    # 0---1
+    # |   |
+    # 2---3
 
-circuit = stim.Circuit("""
-    H 0
-    CX 0 1
-    X_ERROR(0.2) 0 1
-    M 0 1
-    DETECTOR rec[-1] rec[-2]
-""")
+    # Vertex (star) stabilizers
+    circuit.append_operation("MPP", [
+        stim.target_x(0),
+        stim.target_combiner(),
+        stim.target_x(1),
+        stim.target_combiner(),
+        stim.target_x(2),
+    ])
+    circuit.append_operation("MPP", [
+        stim.target_x(1),
+        stim.target_combiner(),
+        stim.target_x(0),
+        stim.target_combiner(),
+        stim.target_x(3),
+    ])
+    circuit.append_operation("MPP", [
+        stim.target_x(2),
+        stim.target_combiner(),
+        stim.target_x(0),
+        stim.target_combiner(),
+        stim.target_x(3),
+    ])
+    circuit.append_operation("MPP", [
+        stim.target_x(3),
+        stim.target_combiner(),
+        stim.target_x(1),
+        stim.target_combiner(),
+        stim.target_x(2),
+    ])
+
+    # Plaquette (face) stabilizers
+    circuit.append_operation("MPP", [
+        stim.target_z(0),
+        stim.target_combiner(),
+        stim.target_z(1),
+        stim.target_combiner(),
+        stim.target_z(2),
+    ])
+    circuit.append_operation("MPP", [
+        stim.target_z(1),
+        stim.target_combiner(),
+        stim.target_z(0),
+        stim.target_combiner(),
+        stim.target_z(3),
+    ])
+    circuit.append_operation("MPP", [
+        stim.target_z(2),
+        stim.target_combiner(),
+        stim.target_z(0),
+        stim.target_combiner(),
+        stim.target_z(3),
+    ])
+    circuit.append_operation("MPP", [
+        stim.target_z(3),
+        stim.target_combiner(),
+        stim.target_z(1),
+        stim.target_combiner(),
+        stim.target_z(2),
+    ])
+
+    return circuit
+
+circuit = generate_2x2_toric_code()
 
 print("--- Circuit ---")
 print(circuit)
